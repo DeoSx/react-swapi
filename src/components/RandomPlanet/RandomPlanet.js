0ce57d2780dcd7e12c15917770da86af5ebcc841
Damir <damir@rocketfirm.com>
2020-02-21 12:28:16 +0600

    feat: add loader

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import SwapiService from '../../services/SwapiService';
-
 import './RandomPlanet.css';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
 export default class RandomPlanet extends Component {
   state = {
-    planet: {}
+    planet: {},
+    loading: true,
+    error: false
   };
 
   constructor() {
@@ -14,45 +17,69 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = planet => {
-    this.setState({planet});
+    this.setState({ planet, loading: false });
+  };
+
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false
+    });
   };
 
   updatePlanet() {
     const swapiPlanet = new SwapiService();
     const id = Math.floor(Math.random() * 25) + 2;
-    swapiPlanet.getPlanet(id).then(this.onPlanetLoaded);
-    console.log(id);
+    // const id = 123123;
+    swapiPlanet
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const {
-      planet: {population, rotationPeriod, diametr, id, name}
-    } = this.state;
+    const { planet, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const hasData = !(loading || errorMessage);
+    const loader = loading ? <Loader /> : null;
+    const content = hasData ? <PlanetData planet={planet} /> : null;
 
     return (
       <div className='random-planet jumbotron rounded'>
-        <img
-          className='planet-image'
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        />
-        <div>
-          <h4>{name}</h4>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>
-              <span className='term'>Population</span>
-              <span>{population}</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Diameter</span>
-              <span>{diametr}</span>
-            </li>
-          </ul>
-        </div>
+        {errorMessage}
+        {loader}
+        {content}
       </div>
     );
   }
 }
+
+const PlanetData = ({ planet }) => {
+  const { population, rotationPeriod, diametr, id, name } = planet;
+
+  return (
+    <React.Fragment>
+      <img
+        className='planet-image'
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+      />
+      <div>
+        <h4>{name}</h4>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <span className='term'>Population</span>
+            <span>{population}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Diameter</span>
+            <span>{diametr}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
